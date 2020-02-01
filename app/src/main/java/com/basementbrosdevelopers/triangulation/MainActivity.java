10a19a3a654fbc087c481ce06281d6429e61480c
Chip Thien <themacrochip@gmail.com>
2020-02-01 14:31:37 -0600

    Triangles are oriented correctly

--- app/src/main/java/com/basementbrosdevelopers/triangulation/MainActivity.java
+++ app/src/main/java/com/basementbrosdevelopers/triangulation/MainActivity.java
@@ -19,16 +19,40 @@ public class MainActivity extends AppCompatActivity {
         LocationMatrix locationMatrix = new LocationMatrix();
         Log.d(getClass().toString(), locationMatrix.toString());
         mainView = findViewById(R.id.main);
-        for (Square[] row : locationMatrix.matrix) {
-            createRow(row);
+        Square[][] matrix = locationMatrix.matrix;
+        for (int i = 0; i < matrix.length; i++) {
+            Square[] row = matrix[i];
+            if (i % 2 == 0) {
+                createRow(row);
+            } else {
+                createInvertedRow(row);
+            }
         }
     }
 
     private void createRow(Square[] row) {
         LinearLayout linearLayout = new LinearLayout(this);
         mainView.addView(linearLayout);
-        for (Square square : row) {
-            createSquare(linearLayout, square);
+        for (int i = 0; i < row.length; i++) {
+            Square square = row[i];
+            if (i % 2 == 0) {
+                createSquare(linearLayout, square);
+            } else {
+                createInvertedSquare(linearLayout, square);
+            }
+        }
+    }
+
+    private void createInvertedRow(Square[] row) {
+        LinearLayout linearLayout = new LinearLayout(this);
+        mainView.addView(linearLayout);
+        for (int i = 0; i < row.length; i++) {
+            Square square = row[i];
+            if (i % 2 == 0) {
+                createInvertedSquare(linearLayout, square);
+            } else {
+                createSquare(linearLayout, square);
+            }
         }
     }
 
@@ -37,15 +61,23 @@ public class MainActivity extends AppCompatActivity {
         createTriangle(linearLayout, R.drawable.ic_triangleorange);
     }
 
-    private void createRotatedTriangle(LinearLayout linearLayout, int drawable) {
-        ImageView triangle = createTriangle(linearLayout, drawable);
-        triangle.setRotation(180f);
+    private void createInvertedSquare(LinearLayout linearLayout, Square square) {
+        ImageView triangle = createTriangle(linearLayout, R.drawable.ic_triangleorange);
+        triangle.setRotation(90f);
+        ImageView rotatedTriangle = createTriangle(linearLayout, R.drawable.ic_triangleorange);
+        rotatedTriangle.setRotation(-90f);
     }
 
     private ImageView createTriangle(ViewGroup parentView, int drawable) {
         ImageView triangle = new ImageView(this);
         triangle.setImageResource(drawable);
         parentView.addView(triangle);
+        return triangle;
+    }
+
+    private ImageView createRotatedTriangle(LinearLayout linearLayout, int drawable) {
+        ImageView triangle = createTriangle(linearLayout, drawable);
+        triangle.setRotation(180f);
         return triangle;
     }
 }
