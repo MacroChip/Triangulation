fbcb834527c8e6c6a8d5db3cfb5c25eb360fcdd3
Chip Thien <themacrochip@gmail.com>
2020-01-31 22:24:20 -0600

    Can programmatically create triangles

--- app/src/main/java/com/basementbrosdevelopers/triangulation/MainActivity.java
+++ app/src/main/java/com/basementbrosdevelopers/triangulation/MainActivity.java
@@ -1,6 +1,8 @@
 package com.basementbrosdevelopers.triangulation;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LocationMatrix.main(null);
+        ViewGroup mainView = findViewById(R.id.main);
+        createTriangle(mainView, R.drawable.ic_triangleorange);
+        createTriangle(mainView, R.drawable.ic_triangleorange);
+    }
+
+    private void createTriangle(ViewGroup mainView, int drawable) {
+        ImageView triangle = new ImageView(this);
+        triangle.setImageResource(drawable);
+        mainView.addView(triangle);
     }
 }
