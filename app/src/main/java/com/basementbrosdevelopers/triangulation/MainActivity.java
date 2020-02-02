059258d2d5b43bc215e9aae9b154309e63f10ee6
Chip Thien <themacrochip@gmail.com>
2020-02-01 22:37:41 -0600

    Short vibrate on triangle swap

--- app/src/main/java/com/basementbrosdevelopers/triangulation/MainActivity.java
+++ app/src/main/java/com/basementbrosdevelopers/triangulation/MainActivity.java
@@ -1,7 +1,11 @@
 package com.basementbrosdevelopers.triangulation;
 
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -12,14 +16,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int DO_NOT_WAIT = 0;
+    public static final int VIBRATION_TIME = 15;
+    public static final int DO_NOT_REPEAT = -1;
     private ViewGroup mainView;
     private Scoreboard allScores = new Scoreboard();
     private LocationMatrix locationMatrix;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         locationMatrix = new LocationMatrix(allScores);
         Log.d(getClass().toString(), locationMatrix.toString());
         mainView = findViewById(R.id.main);
@@ -88,6 +97,15 @@ public class MainActivity extends AppCompatActivity {
             MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.woosh);
             mediaPlayer.setOnCompletionListener(MediaPlayer::release);
             mediaPlayer.start();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    Log.d(getClass().toString(), "Using modern vibrator");
+                    vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK));
+                }
+            } else {
+                Log.d(getClass().toString(), "Using legacy vibrator");
+                vibrator.vibrate(new long[]{DO_NOT_WAIT, VIBRATION_TIME}, DO_NOT_REPEAT);
+            }
             locationMatrix.matrix[y][x].swap();
             locationMatrix.checkLeftRhombus(y, x);
             locationMatrix.checkRightRhombus(y, x);
