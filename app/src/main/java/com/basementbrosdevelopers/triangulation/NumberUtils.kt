21c1969e0a2cd5d79467a9e54e6439f63cb2645d
Chip Thien <themacrochip@gmail.com>
2020-02-01 19:31:10 -0600

    Make some utilities to help with wild triangles

--- app/src/main/java/com/basementbrosdevelopers/triangulation/NumberUtils.kt
+++ app/src/main/java/com/basementbrosdevelopers/triangulation/NumberUtils.kt
@@ -1,2 +1,17 @@
 package com.basementbrosdevelopers.triangulation
 
+fun areAllTheSame(numbers: List<Int>): Boolean {
+    if (numbers.isEmpty()) {
+        return true
+    }
+    val first = numbers[0]
+    return numbers.all { it == first }
+}
+
+fun filterOutWildCards(numbers: List<Int>): List<Int> {
+    return numbers.filter { it != 5 }
+}
+
+fun main() {
+    print(areAllTheSame(filterOutWildCards(listOf(5, 3, 5, 3))))
+}
